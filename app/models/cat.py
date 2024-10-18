e6a227b2fe2dd32616644b01e52ecabe6a307c13
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-10-18 15:40:35 -0700

    adds more cats

--- app/models/cat.py
+++ app/models/cat.py
@@ -11,6 +11,13 @@ cats = [
     Cat(3, "Midnight", "black", "skittish"),
     Cat(4, "Leo", "gray tabby", "friendly"),
     Cat(5, "Ash", "gray", "cranky"),
-    Cat(6, "Alder", "auburn", "jumpy, bumpy, clumpy, thumpy, fun, fun, fun, fun, fun"),
-    Cat(7, "Morty", "orange", "orange")
+    Cat(6, "Alder", "auburn", "bouncy, trouncy, flouncy, pouncy, fun, fun, fun, fun, fun"),
+    Cat(7, "Morty", "orange", "orange"),
+    Cat(8, "fluffy", "white", "evil with a hint of benevolent"),
+    Cat(9, "Reginold", "orange", "only has one brain cell, but is descendent of Reginold the Great Tabby"),
+    Cat(10, "Katosa", "gray tabby", "Crazy Hunter"),
+    Cat(11, "Milly", "Tortoiseshell", "Loves you a lot but will probably sneeze all over you"),
+    Cat(12, "Meryl", "Tortoiseshell", "Bossy but tries to pass as the sweet one"),
+    Cat(13, "Zelda", "white, gray", "a mystery"),
+    Cat(14, "Jupiter", "orange", "socially selective"),
 ]
