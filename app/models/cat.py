caf6e1c1910e82acc1bce2b967073757c768ca84
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-10-18 13:54:21 -0500

    01) Building an API - Intro to Flask, Setup, and Read

---
+++ app/models/cat.py
@@ -0,0 +1,16 @@
+class Cat:
+    def __init__(self, id, name, color, personality):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.personality = personality
+
+cats = [
+    Cat(1, "Luna", "black/white", "lazy"),
+    Cat(2, "Simon", "black", "might be a human stuck in a cats body"),
+    Cat(3, "Midnight", "black", "skittish"),
+    Cat(4, "Leo", "gray tabby", "friendly"),
+    Cat(5, "Ash", "gray", "cranky"),
+    Cat(6, "Alder", "auburn", "jumpy, bumpy, clumpy, thumpy, fun, fun, fun, fun, fun"),
+    Cat(7, "Morty", "orange", "orange")
+]
