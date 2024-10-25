22fc0d588df693985c5170bfb96bddb9c66e19cb
Ashley Yang <ashley.minghui@gmail.com>
2024-10-25 11:21:57 -0700

    Updated Cat model and wrote POST and GET /cats

--- app/models/cat.py
+++ app/models/cat.py
@@ -7,7 +7,13 @@ class Cat(db.Model):
     color: Mapped[str]
     personality: Mapped[str]
 
-
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            color=self.color,
+            personality=self.personality
+        )
 
 
 
--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,7 +1,31 @@
-from flask import Blueprint, abort, make_response
-# from ..models.cat import cats
+from flask import Blueprint, abort, make_response, request
+from ..db import db
+from app.models.cat import Cat
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
+
+@cats_bp.post("")
+def create_cat():
+    request_body = request.get_json()
+    name = request_body["name"]
+    color = request_body["color"]
+    personality = request_body["personality"]
+
+    new_cat = Cat(name=name, color=color, personality=personality)
+    db.session.add(new_cat)
+    db.session.commit()
+
+    response = new_cat.to_dict()
+    return response, 201
+
+@cats_bp.get("")
+def get_all_cats():
+    query = db.select(Cat).order_by(Cat.id)
+    cats = db.session.scalars(query)
+
+    cats_response = [cat.to_dict() for cat in cats]
+    return cats_response
+
 
 # @cats_bp.get("")
 # def get_all_cats():
@@ -29,4 +53,3 @@ def validate_cat(cat_id):
             return cat
     
     abort(make_response({ "message": f"Cat {cat_id} not found"}, 404))
-
