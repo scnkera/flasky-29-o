f3118c5f7638e7c6a2447e7115d15e0188f1a7aa
Ashley Yang <ashley.minghui@gmail.com>
2024-10-25 10:40:18 -0700

    Add sqlalchemy and migrate, make Cat a db backed model, generate migrations

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,21 +1,21 @@
 from flask import Blueprint, abort, make_response
-from ..models.cat import cats
+# from ..models.cat import cats
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 
-@cats_bp.get("")
-def get_all_cats():
-    results_list = []
+# @cats_bp.get("")
+# def get_all_cats():
+#     results_list = []
 
-    for cat in cats:
-        results_list.append(cat.to_dict())
+#     for cat in cats:
+#         results_list.append(cat.to_dict())
     
-    return results_list
+#     return results_list
 
-@cats_bp.get("/<cat_id>")
-def get_one_cat(cat_id):
-    cat = validate_cat(cat_id)
-    return cat.to_dict(), 200
+# @cats_bp.get("/<cat_id>")
+# def get_one_cat(cat_id):
+#     cat = validate_cat(cat_id)
+#     return cat.to_dict(), 200
 
 
 def validate_cat(cat_id):
@@ -29,5 +29,4 @@ def validate_cat(cat_id):
             return cat
     
     abort(make_response({ "message": f"Cat {cat_id} not found"}, 404))
-
 
